eeb4fe16b7be894844fa3997466875a276aa8b8e
Alexander <mscurs@gmail.com>
2023-03-25 18:18:44 +0200

    Add react and babel

---
+++ client/config/root.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Root = () => {
+    return (
+        <div>This is root</div>
+    )
+}
+
+export default Root
---
+++ client/main.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import Root from "./config/root";
+
+const target = document.getElementById('root')
+
+const render = (Component) => {
+    ReactDOM.render(<Component/>, target)
+}
+
+render(Root)
