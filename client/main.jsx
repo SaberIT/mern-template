6952c15aaf7b8e8fcd41eeb1c870ac659ea66923
Alexander <mscurs@gmail.com>
2023-03-27 23:04:09 +0300

    Front

--- client/main.jsx
+++ client/main.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import './assets/styles/style.scss'
 import Root from "./config/root";
 
 const target = document.getElementById('root')
--- server/server.js
+++ server/server.js
@@ -1,10 +1,32 @@
 import express from 'express'
+import { Html } from '../client/html.js'
+import {resolve} from 'path'
 
 const server = express()
 const PORT = 8080
+const __dirname = process.cwd()
+
+const middlewear = [
+    express.static(resolve(__dirname, 'dist'))
+]
+
+middlewear.forEach((it) => server.use(it))
 
 server.get('/', (req, res) => {
     res.send('Express Server')
+})
+
+server.get('/', (req, res) => {
+    const initialState = {
+        location: req.url
+    }
+
+    res.send(
+        Html({
+            body: '',
+            initialState
+        }),
+    )
 })
 
 server.listen(PORT, () => {
